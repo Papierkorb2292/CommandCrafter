091e65011ec0087e07bd224ba48f4d529bbbc6f0
Papierkorb2292 <104673791+Papierkorb2292@users.noreply.github.com>
2023-01-20 14:48:20 +0100

    Start-over commit

---
+++ src/main/kotlin/net/papierkorb2292/command_crafter/CommandCrafter.kt
@@ -0,0 +1,9 @@
+package net.papierkorb2292.command_crafter
+import net.fabricmc.api.ModInitializer
+@Suppress("UNUSED")
+object CommandCrafter: ModInitializer {
+    private const val MOD_ID = "command_crafter"
+    override fun onInitialize() {
+        println("Loaded CommandCrafter!")
+    }
+}
---
+++ src/main/kotlin/net/papierkorb2292/command_crafter/client/ClientCommandCrafter.kt
@@ -0,0 +1,9 @@
+package net.papierkorb2292.command_crafter.client
+
+import net.fabricmc.api.ClientModInitializer
+
+object ClientCommandCrafter : ClientModInitializer {
+    override fun onInitializeClient() {
+
+    }
+}
