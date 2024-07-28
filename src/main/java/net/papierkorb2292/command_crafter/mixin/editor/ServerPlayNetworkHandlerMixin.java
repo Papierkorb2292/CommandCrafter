5f76d48b50d4c931e5173defd4c66e48d205b4a5
Papierkorb2292 <104673791+Papierkorb2292@users.noreply.github.com>
2024-07-28 11:44:27 +0200

    Fixed debugging with suspended server (packets weren't handled asynchronously)

---
+++ src/main/java/net/papierkorb2292/command_crafter/mixin/editor/ServerPlayNetworkHandlerMixin.java
@@ -0,0 +1,43 @@
+package net.papierkorb2292.command_crafter.mixin.editor;
+
+import kotlin.Unit;
+import net.minecraft.network.ClientConnection;
+import net.minecraft.network.packet.c2s.common.CustomPayloadC2SPacket;
+import net.minecraft.network.packet.s2c.common.CustomPayloadS2CPacket;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.network.ConnectedClientData;
+import net.minecraft.server.network.ServerCommonNetworkHandler;
+import net.minecraft.server.network.ServerPlayNetworkHandler;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.papierkorb2292.command_crafter.editor.NetworkServerConnection;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ServerPlayNetworkHandler.class)
+public abstract class ServerPlayNetworkHandlerMixin extends ServerCommonNetworkHandler {
+    @Shadow public ServerPlayerEntity player;
+
+    public ServerPlayNetworkHandlerMixin(MinecraftServer server, ClientConnection connection, ConnectedClientData clientData) {
+        super(server, connection, clientData);
+    }
+
+    @Inject(
+            method = "onCustomPayload",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    private void command_crafter$callNetworkServerConnectionAsyncHandlers(CustomPayloadC2SPacket packet, CallbackInfo ci) {
+        if(NetworkServerConnection.Companion.callPacketHandler(
+                packet.payload(),
+                new NetworkServerConnection.AsyncC2SPacketContext(
+                        player,
+                        connection
+                )
+        )) {
+            ci.cancel();
+        }
+    }
+}
