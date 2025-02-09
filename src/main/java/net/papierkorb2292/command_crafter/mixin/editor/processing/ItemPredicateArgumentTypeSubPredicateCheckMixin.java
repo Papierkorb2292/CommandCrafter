d562fdab4316d8fd18b159f1caa063a5d4721223
Papierkorb2292 <104673791+Papierkorb2292@users.noreply.github.com>
2025-02-09 10:39:21 +0000

    Map target method name in item predicate component condition mixins

--- src/main/java/net/papierkorb2292/command_crafter/mixin/editor/processing/ItemPredicateArgumentTypeSubPredicateCheckMixin.java
+++ src/main/java/net/papierkorb2292/command_crafter/mixin/editor/processing/ItemPredicateArgumentTypeSubPredicateCheckMixin.java
@@ -16,9 +16,9 @@ public class ItemPredicateArgumentTypeSubPredicateCheckMixin {
             method = "createPredicate",
             at = @At(
                     value = "INVOKE",
-                    target = "Lcom/mojang/serialization/Decoder;parse(Lcom/mojang/serialization/DynamicOps;Ljava/lang/Object;)Lcom/mojang/serialization/DataResult;"
-            ),
-            remap = false
+                    target = "Lcom/mojang/serialization/Decoder;parse(Lcom/mojang/serialization/DynamicOps;Ljava/lang/Object;)Lcom/mojang/serialization/DataResult;",
+                    remap = false
+            )
     )
     private Decoder<?> command_crafter$invokeDelayedDecodeNbtAnalyzing(Decoder<?> instance, DynamicOps<?> ops, Object input) {
         final var callback = getOrNull(PackratParserAdditionalArgs.INSTANCE.getDelayedDecodeNbtAnalyzeCallback());
