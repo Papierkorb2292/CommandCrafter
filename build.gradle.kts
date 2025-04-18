b6947ce5cd418d3de06b68e4464e9a3d530182b6
Papierkorb2292 <104673791+Papierkorb2292@users.noreply.github.com>
2025-04-18 06:26:22 +0000

    Added lsp4j license to root directory of final `.jar` file

--- build.gradle.kts
+++ build.gradle.kts
@@ -50,6 +50,7 @@ tasks {
     jar { from("LICENSE") { rename { "${it}_${base.archivesName.get()}" } } }
     jar { from("README.md") }
     jar { from("GSON_LICENSE") }
+    jar { from("LSP4J_LICENSE") }
     processResources {
         filesMatching("fabric.mod.json") { expand(mutableMapOf("version" to project.extra["mod_version"] as String, "fabricloader" to project.extra["loader_version"] as String, "fabric_api" to project.extra["fabric_version"] as String, "fabric_language_kotlin" to project.extra["fabric_language_kotlin_version"] as String, "minecraft" to project.extra["minecraft_version"] as String, "java" to project.extra["java_version"] as String)) }
         filesMatching("*.mixins.json") { expand(mutableMapOf("java" to project.extra["java_version"] as String)) }
