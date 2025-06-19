b0f004018e48da39911c24299a6dd91da26f085f
Papierkorb2292 <104673791+Papierkorb2292@users.noreply.github.com>
2025-06-19 16:29:18 +0000

    Add suggestions for component ids in JSON/NBT

---
+++ src/main/java/net/papierkorb2292/command_crafter/mixin/editor/processing/ComponentChangesTypeMixin.java
@@ -0,0 +1,34 @@
+package net.papierkorb2292.command_crafter.mixin.editor.processing;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.DynamicOps;
+import net.minecraft.registry.Registries;
+import net.papierkorb2292.command_crafter.editor.processing.CodecSuggestionWrapper;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+import java.util.stream.Stream;
+
+@Mixin(targets = "net.minecraft.component.ComponentChanges$Type")
+public class ComponentChangesTypeMixin {
+    @ModifyExpressionValue(
+            method = "<clinit>",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lcom/mojang/serialization/codecs/PrimitiveCodec;flatXmap(Ljava/util/function/Function;Ljava/util/function/Function;)Lcom/mojang/serialization/Codec;",
+                    remap = false
+            )
+    )
+    private static Codec<Object> command_crafter$addComponentSuggestions(Codec<Object> original) {
+        return new CodecSuggestionWrapper<>(original, ComponentChangesTypeMixin::command_crafter$generateComponentSuggestions);
+    }
+
+    private static <T> Stream<T> command_crafter$generateComponentSuggestions(DynamicOps<T> ops) {
+        final var ids = Registries.DATA_COMPONENT_TYPE.getIds();
+        return Stream.concat(
+                ids.stream().map(id -> ops.createString("!" + id.toString())),
+                ids.stream().map(id -> ops.createString(id.toString()))
+        );
+    }
+}
