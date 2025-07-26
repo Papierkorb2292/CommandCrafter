b6c328b53dd37a3169ce1a653cd2572725ea43a8
Papierkorb2292 <104673791+Papierkorb2292@users.noreply.github.com>
2025-07-26 23:15:43 +0200

    Added test for cursor mapper from `MacroInvocationMixin`

--- src/main/kotlin/net/papierkorb2292/command_crafter/test/TestCommandCrafter.kt
+++ src/main/kotlin/net/papierkorb2292/command_crafter/test/TestCommandCrafter.kt
@@ -1,9 +1,12 @@
 package net.papierkorb2292.command_crafter.test
 
 import net.fabricmc.fabric.api.gametest.v1.GameTest
+import net.minecraft.command.MacroInvocation
 import net.minecraft.test.TestContext
 import net.minecraft.text.Text
-import net.papierkorb2292.command_crafter.parser.FileMappingInfo
+import net.papierkorb2292.command_crafter.helper.IntList
+import net.papierkorb2292.command_crafter.helper.IntList.Companion.intListOf
+import net.papierkorb2292.command_crafter.parser.helper.MacroCursorMapperProvider
 import net.papierkorb2292.command_crafter.test.TestSnapshotHelper.assertEqualsSnapshot
 import org.spongepowered.asm.mixin.MixinEnvironment
 
@@ -24,5 +27,16 @@ object TestCommandCrafter {
     fun auditMixins(context: TestContext) {
         MixinEnvironment.getCurrentEnvironment().audit()
         context.complete()
+    }
+
+    @GameTest
+    fun testMacroInvocationCursorMapper(context: TestContext) {
+        val macroInvocation = MacroInvocation.parse("say $(greeting), $(name)!")
+        val arguments = listOf("What's up", "your highness")
+        @Suppress("CAST_NEVER_SUCCEEDS")
+        val cursorMapper = (macroInvocation as MacroCursorMapperProvider).`command_crafter$getCursorMapper`(arguments)
+        context.assertEquals(intListOf("".length, "say $(greeting)".length, "say $(greeting), $(name)".length), cursorMapper.sourceCursors,Text.literal("source_cursors"))
+        context.assertEquals(intListOf("".length, "say What's up".length, "say What's up, your highness".length), cursorMapper.targetCursors, Text.literal("target_cursors"))
+        context.assertEquals(intListOf("say ".length, ", ".length, "!".length), cursorMapper.lengths, Text.literal("lengths"))
     }
 }
