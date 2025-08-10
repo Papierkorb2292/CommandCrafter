af8c8394524ace625676aedc3696971a9e2aa02c
Papierkorb2292 <104673791+Papierkorb2292@users.noreply.github.com>
2025-08-10 12:58:24 +0200

    Added snapshot tests for function parser

--- src/main/kotlin/net/papierkorb2292/command_crafter/test/TestCommandCrafter.kt
+++ src/main/kotlin/net/papierkorb2292/command_crafter/test/TestCommandCrafter.kt
@@ -1,13 +1,29 @@
 package net.papierkorb2292.command_crafter.test
 
+import com.mojang.brigadier.CommandDispatcher
+import com.mojang.brigadier.context.StringRange
+import com.mojang.brigadier.exceptions.CommandSyntaxException
 import net.fabricmc.fabric.api.gametest.v1.GameTest
 import net.minecraft.command.MacroInvocation
-import net.minecraft.test.TestContext
-import net.minecraft.text.Text
-import net.papierkorb2292.command_crafter.helper.IntList
 import net.papierkorb2292.command_crafter.helper.IntList.Companion.intListOf
 import net.papierkorb2292.command_crafter.parser.helper.MacroCursorMapperProvider
+import net.minecraft.command.CommandSource
+import net.minecraft.test.TestContext
+import net.minecraft.text.Text
+import net.minecraft.util.Identifier
+import net.minecraft.util.math.Vec3d
+import net.papierkorb2292.command_crafter.editor.processing.AnalyzingResourceCreator
+import net.papierkorb2292.command_crafter.editor.processing.helper.AnalyzingResult
+import net.papierkorb2292.command_crafter.parser.DirectiveStringReader
+import net.papierkorb2292.command_crafter.parser.FileMappingInfo
+import net.papierkorb2292.command_crafter.parser.Language
+import net.papierkorb2292.command_crafter.parser.LanguageManager
+import net.papierkorb2292.command_crafter.parser.ParsedResourceCreator
+import net.papierkorb2292.command_crafter.parser.RawZipResourceCreator
+import net.papierkorb2292.command_crafter.parser.helper.RawResource
+import net.papierkorb2292.command_crafter.parser.languages.VanillaLanguage
 import net.papierkorb2292.command_crafter.test.TestSnapshotHelper.assertEqualsSnapshot
+import org.eclipse.lsp4j.Position
 import org.spongepowered.asm.mixin.MixinEnvironment
 
 object TestCommandCrafter {
@@ -39,5 +55,84 @@ object TestCommandCrafter {
         context.assertEquals(intListOf("".length, "say What's up".length, "say What's up, your highness".length), cursorMapper.targetCursors, Text.literal("target_cursors"))
         context.assertEquals(intListOf("say ".length, ", ".length, "!".length), cursorMapper.lengths, Text.literal("lengths"))
         context.complete()
+    }
+
+    @GameTest
+    fun parseTestFunction(context: TestContext) {
+        val lines = """
+            # This is a doc \    
+                comment
+            execute as @a run say HI!
+            execute if entity @s \ 
+                run tp @s ~ ~1 ~
+            ${'$'}say hi to $(the_macro) \  
+                for me
+            @language vanilla improved
+            execute
+                align xyz run
+                summon minecraft:armor_stand 
+        """.trimIndent().lines()
+        try {
+            testAllFunctionParsers(lines, context)
+        } catch(e: CommandSyntaxException) {
+            context.throwGameTestException(Text.literal("Error testing function parsers: ${e.message}"))
+        }
+        context.complete()
+    }
+
+    fun testAllFunctionParsers(lines: List<String>, context: TestContext) {
+        val id = Identifier.of("test")
+        @Suppress("UNCHECKED_CAST")
+        val commandDispatcher = context.world.server.commandManager.dispatcher as CommandDispatcher<CommandSource>
+        val source = context.world.server.commandSource
+            .withPosition(Vec3d.ZERO) // Default position is the worldspawn, which changes between test so it must be set to another value
+
+        val parsedResourceCreator = ParsedResourceCreator(
+            id,
+            "testPack"
+        )
+        val originResource = parsedResourceCreator.addOriginResource()
+        val parsed = LanguageManager.parseToCommands(
+            DirectiveStringReader(
+                FileMappingInfo(lines),
+                commandDispatcher,
+                parsedResourceCreator
+            ),
+            source,
+            Language.TopLevelClosure(VanillaLanguage())
+        )
+        val characterCount = lines.sumOf { it.length + 1 } - 1 // With newlines after every line except for the last
+        originResource(ParsedResourceCreator.ResourceStackInfo(id, StringRange(0, characterCount)))
+        parsedResourceCreator.popOriginResource()
+        context.assertEqualsSnapshot(parsed.toCommandFunction(id), "parseToCommands_actions")
+
+        val zipResourceCreator = RawZipResourceCreator()
+        val parseToVanillaResource = RawResource(RawResource.FUNCTION_TYPE)
+        LanguageManager.parseToVanilla(
+            DirectiveStringReader(
+                FileMappingInfo(lines),
+                commandDispatcher,
+                zipResourceCreator
+            ),
+            source,
+            parseToVanillaResource,
+            Language.TopLevelClosure(VanillaLanguage())
+        )
+        context.assertEqualsSnapshot(parseToVanillaResource.content, "parse_to_vanilla")
+        val analyzingResult = AnalyzingResult(FileMappingInfo(lines), Position())
+        LanguageManager.analyse(
+            DirectiveStringReader(
+                analyzingResult.mappingInfo,
+                commandDispatcher,
+                AnalyzingResourceCreator(
+                    null,
+                    "testPack/data/minecraft/function/test.mcfunction"
+                )
+            ),
+            source,
+            analyzingResult,
+            Language.TopLevelClosure(VanillaLanguage())
+        )
+        context.assertEqualsSnapshot(analyzingResult, "analyzing_result")
     }
 }
