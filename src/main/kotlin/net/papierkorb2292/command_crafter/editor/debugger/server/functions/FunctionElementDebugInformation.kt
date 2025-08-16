23e149868ac03cdaa93c9b4bfdf96da6092ed8cc
Papierkorb2292 <104673791+Papierkorb2292@users.noreply.github.com>
2025-08-16 16:19:55 +0200

    Fixed `argumentBreakpointParserSuppliers` not being excluded from parser snapshot tests, leading to failing snapshots because it's contents don't necessarily implement toString and are serialized with their object id

--- src/main/kotlin/net/papierkorb2292/command_crafter/editor/debugger/server/functions/FunctionElementDebugInformation.kt
+++ src/main/kotlin/net/papierkorb2292/command_crafter/editor/debugger/server/functions/FunctionElementDebugInformation.kt
@@ -12,7 +12,7 @@ import net.minecraft.server.command.CommandOutput
 import net.minecraft.server.command.ServerCommandSource
 import net.minecraft.server.function.Macro
 import net.minecraft.util.Identifier
-import net.minidev.json.annotate.JsonIgnore
+import com.fasterxml.jackson.annotation.JsonIgnore
 import net.papierkorb2292.command_crafter.editor.PackagedId
 import net.papierkorb2292.command_crafter.editor.debugger.DebugPauseHandler
 import net.papierkorb2292.command_crafter.editor.debugger.MinecraftDebuggerServer
