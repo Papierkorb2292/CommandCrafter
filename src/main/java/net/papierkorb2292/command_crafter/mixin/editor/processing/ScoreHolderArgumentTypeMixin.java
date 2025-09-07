1f7c91676e3058c8b14f0f9a573ffa792168389c
Papierkorb2292 <104673791+Papierkorb2292@users.noreply.github.com>
2025-09-07 19:47:59 +0200

    Fixed missing semantic tokens when `ScoreHolderArgumentType` contained characters that the entity selector reader doesn't allow

--- src/main/java/net/papierkorb2292/command_crafter/mixin/editor/processing/ScoreHolderArgumentTypeMixin.java
+++ src/main/java/net/papierkorb2292/command_crafter/mixin/editor/processing/ScoreHolderArgumentTypeMixin.java
@@ -7,6 +7,7 @@ import net.minecraft.command.CommandSource;
 import net.minecraft.command.EntitySelectorReader;
 import net.minecraft.command.argument.ScoreHolderArgumentType;
 import net.papierkorb2292.command_crafter.editor.processing.AnalyzingResourceCreator;
+import net.papierkorb2292.command_crafter.editor.processing.TokenType;
 import net.papierkorb2292.command_crafter.editor.processing.helper.AnalyzingCommandNode;
 import net.papierkorb2292.command_crafter.editor.processing.helper.AnalyzingResult;
 import net.papierkorb2292.command_crafter.editor.processing.helper.AnalyzingResultDataContainer;
@@ -19,8 +20,12 @@ public class ScoreHolderArgumentTypeMixin implements AnalyzingCommandNode {
 
     @Override
     public void command_crafter$analyze(@NotNull CommandContext<CommandSource> context, @NotNull StringRange range, @NotNull DirectiveStringReader<AnalyzingResourceCreator> reader, @NotNull AnalyzingResult result, @NotNull String name) throws CommandSyntaxException {
-        var selectorReader = new EntitySelectorReader(reader, true);
-        ((AnalyzingResultDataContainer)selectorReader).command_crafter$setAnalyzingResult(result);
-        selectorReader.read();
+        if(reader.peek() == '@') {
+            var selectorReader = new EntitySelectorReader(reader, true);
+            ((AnalyzingResultDataContainer) selectorReader).command_crafter$setAnalyzingResult(result);
+            selectorReader.read();
+        } else {
+            result.getSemanticTokens().addMultiline(range, TokenType.Companion.getPARAMETER(), 0);
+        }
     }
 }
