3d83f599424f92aa3614faa7188bb39e0e6e1d85
Papierkorb2292 <104673791+Papierkorb2292@users.noreply.github.com>
2025-10-25 12:02:45 +0200

    Fixed `#` character at the start of item predicates not being highlighted

--- src/main/java/net/papierkorb2292/command_crafter/mixin/editor/processing/TagParsingRuleMixin.java
+++ src/main/java/net/papierkorb2292/command_crafter/mixin/editor/processing/TagParsingRuleMixin.java
@@ -1,12 +1,21 @@
 package net.papierkorb2292.command_crafter.mixin.editor.processing;
 
+import net.minecraft.command.argument.ItemPredicateParsing;
+import net.minecraft.util.packrat.ParsingRuleEntry;
 import net.papierkorb2292.command_crafter.editor.processing.PackContentFileType;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(targets = "net/minecraft/command/argument/ItemPredicateParsing$TagParsingRule")
 public class TagParsingRuleMixin extends IdentifiableParsingRuleMixin {
 
-    public TagParsingRuleMixin() {
+    @Inject(
+            method = "<init>",
+            at = @At("TAIL")
+    )
+    private void command_crafter$initIdAnalyzing(ParsingRuleEntry<?, ?> idParsingRule, ItemPredicateParsing.Callbacks<?, ?, ?> callbacks, CallbackInfo ci) {
         command_crafter$setStartOffset(-1);
         command_crafter$setPackContentFileType(PackContentFileType.ITEM_TAGS_FILE_TYPE);
     }
