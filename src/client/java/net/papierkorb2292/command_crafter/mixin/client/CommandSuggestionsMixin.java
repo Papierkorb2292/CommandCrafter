b53095f7a22beacc89bb366094a48d716db70073
Papierkorb2292 <104673791+Papierkorb2292@users.noreply.github.com>
2026-02-17 07:37:57 +0100

    Updated `CommandSuggestionsMixin`

--- src/client/java/net/papierkorb2292/command_crafter/mixin/client/CommandSuggestionsMixin.java
+++ src/client/java/net/papierkorb2292/command_crafter/mixin/client/CommandSuggestionsMixin.java
@@ -1,6 +1,5 @@
 package net.papierkorb2292.command_crafter.mixin.client;
 
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.injector.ModifyReceiver;
 import com.mojang.brigadier.suggestion.Suggestions;
 import net.minecraft.client.Minecraft;
@@ -9,6 +8,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.function.Consumer;
 import java.util.function.Function;
 
 @Mixin(CommandSuggestions.class)
@@ -18,10 +18,10 @@ public class CommandSuggestionsMixin {
             method = "updateCommandInfo",
             at = @At(
                     value = "INVOKE",
-                    target = "Ljava/util/concurrent/CompletableFuture;thenRun(Ljava/lang/Runnable;)Ljava/util/concurrent/CompletableFuture;"
+                    target = "Ljava/util/concurrent/CompletableFuture;thenAccept(Ljava/util/function/Consumer;)Ljava/util/concurrent/CompletableFuture;"
             )
     )
-    private CompletableFuture<Suggestions> command_crafter$ensureCompletionFutureRunsOnRenderThread(CompletableFuture<Suggestions> instance, Runnable action) {
+    private CompletableFuture<Suggestions> command_crafter$ensureCompletionFutureRunsOnRenderThread(CompletableFuture<Suggestions> instance, Consumer<?> consumer) {
         // This is necessary, because UnicodeNameSuggestionSupplier could have switched the future to a different thread,
         // which would throw an error in showSuggestions at this.font.width
         return instance.thenApplyAsync(Function.identity(), Minecraft.getInstance());
