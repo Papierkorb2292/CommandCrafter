6ed06082cfddd1d8011f93f1ac5f2526f9fc6b3f
Papierkorb2292 <104673791+Papierkorb2292@users.noreply.github.com>
2026-03-14 07:21:00 +0100

    Removed analyzing error for `BlockAttachedEntity` missing `block_pos`, since Minecraft can actually still spawn the entity

--- src/main/java/net/papierkorb2292/command_crafter/mixin/editor/processing/BlockAttachedEntityMixin.java
+++ src/main/java/net/papierkorb2292/command_crafter/mixin/editor/processing/BlockAttachedEntityMixin.java
@@ -1,14 +1,11 @@
 package net.papierkorb2292.command_crafter.mixin.editor.processing;
 
 import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
-import com.mojang.serialization.DataResult;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.decoration.BlockAttachedEntity;
-import net.minecraft.world.level.storage.ValueInput;
 import net.minecraft.world.level.Level;
-import net.papierkorb2292.command_crafter.editor.processing.DynamicOpsReadView;
-import net.papierkorb2292.command_crafter.editor.processing.PreLaunchDecoderOutputTracker;
+import net.minecraft.world.level.storage.ValueInput;
 import net.papierkorb2292.command_crafter.helper.DummyWorld;
 import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.Mixin;
@@ -30,17 +27,6 @@ public abstract class BlockAttachedEntityMixin extends Entity {
             allow = 1
     )
     private boolean command_crafter$suppressMissingTeamWarnWhenAnalyzing(Logger instance, String s, Object o, ValueInput view) {
-        if(this.level() instanceof DummyWorld) {
-            if(!(view instanceof DynamicOpsReadView<?> dynamicOpsReadView)) return false;
-            // Error is only added when block pos is null. The other possible case is that the block pos is too far away from the entities position,
-            // but this can't be checked when analyzing since relative coordinates might be used in the command.
-            if(o != null) return false;
-            PreLaunchDecoderOutputTracker.INSTANCE.onDecoded(
-                    DataResult.error(() -> s.replace("{}", "null")),
-                    dynamicOpsReadView.getDynamic().getValue()
-            );
-            return false;
-        }
-        return true;
+        return !(this.level() instanceof DummyWorld);
     }
 }
