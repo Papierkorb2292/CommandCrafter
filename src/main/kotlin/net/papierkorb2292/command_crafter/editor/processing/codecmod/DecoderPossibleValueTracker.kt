6b79264991f4266cb98f4b438f833fc567fdc173
Papierkorb2292 <104673791+Papierkorb2292@users.noreply.github.com>
2026-04-02 11:02:40 +0200

    Use `IdentityHashMap` in `DecoderPossibleValueTracker`

--- src/main/kotlin/net/papierkorb2292/command_crafter/editor/processing/codecmod/DecoderPossibleValueTracker.kt
+++ src/main/kotlin/net/papierkorb2292/command_crafter/editor/processing/codecmod/DecoderPossibleValueTracker.kt
@@ -1,10 +1,12 @@
 package net.papierkorb2292.command_crafter.editor.processing.codecmod
 
+import java.util.*
+
 class DecoderPossibleValueTracker<TNode : Any> : ExtraDecoderBehavior<TNode> {
     override val branchBehavior: ExtraDecoderBehavior.BranchBehavior
         get() = ExtraDecoderBehavior.BranchBehavior.forAllPossibleEncoded(ExtraDecoderBehavior.NonCanonicalBehavior.KEEP_BRANCH_BEHAVIOR)
 
-    val possibleValues = mutableMapOf<TNode, MutableList<TNode>>()
+    val possibleValues = IdentityHashMap<TNode, MutableList<TNode>>()
 
     override fun notePossibleValues(
         input: TNode,
