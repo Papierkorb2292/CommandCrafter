0872f5ae0eaa9b896ae93d2f0afbb790b2c203c4
Papierkorb2292 <104673791+Papierkorb2292@users.noreply.github.com>
2026-08-09 11:23:21 +0200

    Filter Unicode name suggestions with `UNICODE_NAME` pattern from `SnbtGrammar`, since only matching names are allowed by the parser

--- src/main/kotlin/net/papierkorb2292/command_crafter/editor/processing/helper/UnicodeNameSuggestionSupplier.kt
+++ src/main/kotlin/net/papierkorb2292/command_crafter/editor/processing/helper/UnicodeNameSuggestionSupplier.kt
@@ -5,6 +5,8 @@ import com.mojang.brigadier.Message
 import com.mojang.brigadier.StringReader
 import com.mojang.brigadier.suggestion.Suggestions
 import com.mojang.brigadier.suggestion.SuggestionsBuilder
+import it.unimi.dsi.fastutil.chars.CharSet
+import it.unimi.dsi.fastutil.chars.CharSets
 import net.minecraft.commands.SharedSuggestionProvider
 import net.minecraft.util.parsing.packrat.ParseState
 import net.minecraft.util.parsing.packrat.SuggestionSupplier
@@ -12,11 +14,17 @@ import java.util.concurrent.CompletableFuture
 import java.util.stream.Stream
 
 object UnicodeNameSuggestionSupplier : SuggestionSupplier<StringReader> {
+    private val allowedNameChars = CharSet.of(
+        *(charSetFromToInclusive('a', 'z')
+                    + charSetFromToInclusive('A', 'Z')
+                    + charSetFromToInclusive('0', '9')
+                    + CharSets.singleton('-')
+            ).toCharArray()) // From SnbtGrammar.UNICODE_NAME
     private val characters: List<Pair<String, Message>> by lazy {
         (Character.MIN_CODE_POINT..Character.MAX_CODE_POINT)
             .asSequence()
             .map { Character.getName(it) to LiteralMessage(Character.toString(it)) }
-            .filter { it.first != null }
+            .filter { it.first != null && it.first.all { c -> c in allowedNameChars } }
             .toMutableList()
             .apply { sortBy { it.first } } // Sort now so Minecraft doesn't have to do so repeatedly
     }
@@ -29,4 +37,6 @@ object UnicodeNameSuggestionSupplier : SuggestionSupplier<StringReader> {
             SharedSuggestionProvider.suggest(characters, suggestionsBuilder, Pair<String, Message>::first, Pair<String, Message>::second)
         }.thenCompose { it }
     }
+
+    private fun charSetFromToInclusive(from: Char, to: Char) = CharSets.fromTo(from, (to.code + 1).toChar())
 }
